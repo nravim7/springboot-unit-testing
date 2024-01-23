3436c0abd4ee4ed89d9e2edb5e22a5470ef4f318
Ravi N <your.email@example.com>
2024-01-23 21:14:09 +0530

    90 - @SpringBootTest : DEFINED_PORT number

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,7 +1,15 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+properties = {"server.port=8081","hostname=192.168.0.2"})
 public class UsersControllerIntegrationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
 }
